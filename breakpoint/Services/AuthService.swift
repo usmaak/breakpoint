b2f2de405f52f632a4b7d625d451235ed2dad6a6
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-11-24 22:02:10 -0700

    Added authentication

---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,37 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by Scott Kilbourn on 11/24/17.
+//  Copyright © 2017 Scott Kilbourn. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?)->()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?)->()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+        }
+    }
+}
