c156022ab16baeff206cc8086d67cb6e8d298c1c
Scott Kilbourn <scottkilbourn@Scotts-MacBook-Pro.local>
2017-11-25 21:39:32 -0700

    Added PostVC

---
+++ breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,51 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by Scott Kilbourn on 11/25/17.
+//  Copyright © 2017 Scott Kilbourn. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
+
+class CreatePostVC: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textView.delegate = self
+    }
+
+    @IBAction func closeButtonWasPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendButtonWasPressed(_ sender: UIButton) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendButton.isEnabled = false
+            
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (complete) in
+                if complete {
+                    self.dismiss(animated: true, completion: nil)
+                }
+                else {
+                    print("There was an error.")
+                }
+
+                self.sendButton.isEnabled = true
+            })
+        }
+    }
+    
+}
--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -36,4 +36,14 @@ class DataService {
         //Adds a user to users.
         REF_USERS.child(uid).updateChildValues(userData)
     }
+    
+    func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool)->()) {
+        if groupKey != nil {
+            //Send to groups ref
+        }
+        else {
+            REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
+            sendComplete(true)
+        }
+    }
 }
